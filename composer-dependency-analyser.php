b393b9979d6b50aafb9be6bc1913e11a906c097e
Jan Nedbal <janedbal@gmail.com>
2024-01-12 11:45:58 +0100

    Replace composer-unused & composer-require-checker with composer-dependency-analyser (#81)
    * Replace composer-unused & composer-require-checker with composer-dependency-analyser
    
    * Add to CI

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+$config = new Configuration();
+
+$ciTools = [
+    'editorconfig-checker/editorconfig-checker',
+    'ergebnis/composer-normalize',
+    'phpstan/phpstan',
+    'phpstan/phpstan-phpunit',
+    'phpstan/phpstan-strict-rules',
+    'shipmonk/composer-dependency-analyser',
+    'shipmonk/phpstan-rules',
+    'slevomat/coding-standard',
+];
+
+return $config
+    ->enableAnalysisOfUnusedDevDependencies()
+    ->ignoreErrorsOnPackages($ciTools, [ErrorType::UNUSED_DEPENDENCY]);
