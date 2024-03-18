1864b6bc0153fc40197f0831630a0cf385232e01
Jan Nedbal <janedbal@gmail.com>
2024-03-18 09:04:20 +0100

    Support doctrine/orm 3.0

--- tests/Doctrine/MySql/Account.php
+++ tests/Doctrine/MySql/Account.php
@@ -2,28 +2,28 @@
 
 namespace ShipMonk\Doctrine\MySql;
 
-use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
 
-/**
- * @ORM\Table(name=Account::TABLE_NAME)
- * @ORM\Entity
- */
+#[Table(name: Account::TABLE_NAME)]
+#[Entity]
 class Account
 {
 
     public const TABLE_NAME = 'account';
 
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="string", nullable=false)
-     * @ORM\GeneratedValue
-     */
+    #[Id]
+    #[Column(type: 'string', nullable: false)]
+    #[GeneratedValue]
     public int $id;
 
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="managedAccounts")
-     * @ORM\JoinColumn(nullable=false)
-     */
+    #[ManyToOne(targetEntity: User::class, inversedBy: 'managedAccounts')]
+    #[JoinColumn(nullable: false)]
     public User $manager;
 
     public function __construct(int $id, User $manager)
